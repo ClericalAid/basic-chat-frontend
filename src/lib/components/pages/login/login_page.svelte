383630090b1759ec1d9a5dc2020dfde3b34f494d
christian <christian.lee93@gmail.com>
2024-01-26 01:47:10 -0500

    Cleanup login page. Spoof login

--- src/lib/components/pages/login/login_page.svelte
+++ src/lib/components/pages/login/login_page.svelte
@@ -1,26 +1,28 @@
-<script>
+<script lang="ts">
   import { goto } from "$app/navigation";
   let username = '';
 
-  function login(){
+  async function login(){
+    await sleep(2000);
     goto("/chat");
+  }
+
+  function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 </script>
 
-<div class="flex flex-row h-screen w-screen items-center justify-center">
-  <div class="flex flex-col w-96 h-[450px] bg-brown-800 items-center justify-center rounded-lg">
-    <div class="flex justify-center items-center">
-      <div class="flex flex-col w-full text-stone-200 items-center justify-center">
-        <div class="w-fit text-2xl pb-3">
-          <b>Enter Username</b>
-        </div>
-        <div class="pb-3">
-          <input bind:value={username} class="bg-brown-950 w-80 rounded-md h-10"/>
-        </div>
-        <button on:click={login} class="bg-rose-300 h-11 w-80 text-lg text-brown-900 rounded-md hover:bg-rose-200 active:bg-rose-100">
-          Login
-        </button>
-      </div>
+<div class="flex h-screen w-screen items-center justify-center">
+  <div class="flex flex-col w-96 py-10 bg-brown-800 items-center justify-center rounded-lg">
+    <div class="w-fit text-stone-200 text-2xl pb-5">
+      <b>Enter Username</b>
     </div>
+    <div class="flex-col w-80 pb-3">
+      <div class="pb-1">Username</div>
+      <input bind:value={username} class="bg-brown-950 w-80 rounded-md h-10"/>
+    </div>
+    <button on:click={login} class="bg-rose-300 h-11 w-80 text-lg text-brown-900 rounded-md hover:bg-rose-200 active:bg-rose-100">
+      Login
+    </button>
   </div>
 </div>
